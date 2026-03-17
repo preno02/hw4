1f9d14d3441bf7a18c3a23251cb70bd6977bfd57
preno02 <parker.reno@kellogg.northwestern.edu>
2026-03-17 22:58:58 +0000

    Enter images in new post and show

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,7 +2,12 @@ class EntriesController < ApplicationController
   
   def index
     # filter entries by logged-in user
-    @entry = entry.where({ "user_id" => session["user_id"] })
+      # @entries = Entry.where({ "user_id" => session["user_id"] })
+       @entries = Entry.where({ "user_id" => session["user_id"], "place_id" => params["place_id"] })
+  end
+  
+  def show
+  @entry = Entry.find_by({ "id" => params["id"], "user_id" => session["user_id"] })
   end
   
   def new
@@ -17,7 +22,11 @@ class EntriesController < ApplicationController
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = session["user_id"]
     @entry.save
+
+    if params["uploaded_image"].present?
+      @entry.uploaded_image.attach(params["uploaded_image"])
+    end
+
     redirect_to "/places/#{@entry["place_id"]}"
   end
-
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       # 3. if they know their password -> login is successful
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
-        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        flash["notice"] = "Welcome, #{@user["email"]}."
         redirect_to "/places"
       else
         # 4b. if the user doesn't know their password -> login fails
