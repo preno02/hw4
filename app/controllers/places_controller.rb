af216293cfac351e6d85ec742e317b0b8406e510
preno02 <parker.reno@kellogg.northwestern.edu>
2026-03-17 03:42:25 +0000

    Front End Styling

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,7 +15,7 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["name"]
-    @place["user_id"] = session["user_id"]
+
     @place.save
     redirect_to "/places"
   end
